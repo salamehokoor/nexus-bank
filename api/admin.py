f921779cc50827a2ce6b1814be7664c8bdaaea37
salameh basil okoor <salamehokoor@gmail.com>
2025-11-06 19:41:02 +0300

    added Cards to the admin register

--- api/admin.py
+++ api/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from . import models
 
 # This "registers" your models with the admin site
+
 admin.site.register(models.User)
 admin.site.register(models.Account)
+admin.site.register(models.Card)
