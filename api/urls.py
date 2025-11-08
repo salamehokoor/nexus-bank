4cd6282ec2be500fbb0611e281486784e960b086
salameh basil okoor <salamehokoor@gmail.com>
2025-11-09 00:48:54 +0300

    trying to merge the two branches

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 # api/urls.py
 
 from django.urls import path
+from .views import AccountsListCreateView, AccountCardsListCreateView
+from .views import GoogleLogin
 from .views import AccountsListCreateView, AccountCardsListCreateView, InternalTransferListCreateView, ExternalTransferListCreateView
 
 urlpatterns = [
@@ -8,6 +10,7 @@ urlpatterns = [
     path("accounts/<str:account_number>/cards/",
          AccountCardsListCreateView.as_view(),
          name="account-cards"),
+    path('auth/google/', GoogleLogin.as_view(), name='google_login'),
     path("transfers/internal/",
          InternalTransferListCreateView.as_view(),
          name="transfer-internal"),
