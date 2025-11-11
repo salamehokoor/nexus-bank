8733f8ff0c14275fad6d0b344cde2a35119ce383
salameh basil okoor <salamehokoor@gmail.com>
2025-11-11 17:14:57 +0300

    changes in the settings

--- nexus/settings.py
+++ nexus/settings.py
@@ -172,6 +172,11 @@ USE_TZ = True
 # STATIC FILES
 # --------------------
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # --------------------
