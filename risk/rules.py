7fee71cb31fedb953a5a9f9531adfb7688df6877
salameh basil okoor <salamehokoor@gmail.com>
2025-11-18 02:54:41 +0300

    Trying to get the country from the ip

--- risk/rules.py
+++ risk/rules.py
@@ -1,4 +1,5 @@
 # risk/rules.py
+from risk.utils import get_country_from_ip
 from .models import Incident
 
 
@@ -21,7 +22,7 @@ def high_value_transfer(request, transfer, threshold=1000):
     Incident.objects.create(
         user=request.user if request.user.is_authenticated else None,
         ip=ip,
-        country="",  # fill later when you add IP->country
+        country=get_country_from_ip(ip),  # fill later when you add IP->country
         event=f"High value transfer: {transfer.amount}",
         severity="high",
         details={
